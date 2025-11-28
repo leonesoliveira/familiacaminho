f3940f5a62d505ed13bbc6763ce3e59d4932aa71
Leones <leonesa.azrael@gmail.com>
2025-11-28 10:41:10 -0300

    logo update

--- src/components/icons/LogoIcon.tsx
+++ src/components/icons/LogoIcon.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import logoSrc from "./logo.svg"; // O Next entende isso como objeto da imagem
-
-export const LogoIcon = ({ className, ...props }: React.ComponentProps<typeof Image>) => (
-  <Image 
-    src={logoSrc} 
-    alt="Logo" 
-    className={className}
+export const LogoIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    // IMPORTANTE: Copie o viewBox do seu arquivo logo.svg original e cole abaixo
+    viewBox="0 0 100 100" 
+    // Isso permite que você passe className, fill, width, etc. via props
     {...props} 
-  />
+  >
+    {/* --- COLE O CONTEÚDO DO SEU LOGO.SVG AQUI ABAIXO --- */}
+    {/* Exemplo: <path d="M10 10 H 90 V 90 H 10 L 10 10" fill="currentColor" /> */}
+    
+  </svg>
 );
